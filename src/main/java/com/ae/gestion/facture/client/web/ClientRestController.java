c2bfc0b41094eb9e00fb6f51469421306abacc11
Abdelouahedd <ennouriabdelouahed@gmail.com>
2021-12-12 19:18:40 +0100

    feat(client): add default sort by id to get page of clients

--- src/main/java/com/ae/gestion/facture/client/web/ClientRestController.java
+++ src/main/java/com/ae/gestion/facture/client/web/ClientRestController.java
@@ -8,7 +8,9 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,7 +38,7 @@ public class ClientRestController {
     @Or({
       @Spec(path = "nom", params = "q", spec = LikeIgnoreCase.class),
       @Spec(path = "prenom", params = "q", spec = LikeIgnoreCase.class)
-    }) Specification<Client> specification, Pageable pageable) {
+    }) Specification<Client> specification,@PageableDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
     Page<Client> client = this.clientService.getClient(specification, pageable);
     return ResponseEntity.ok(client);
   }
